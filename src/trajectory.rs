89d71c22580f45bfe3a8a07ade1068fff4adf59b
samiBendou <sami@dahoux.net>
2020-01-24 14:19:14 +0100

    Writing trajectory documentation

--- src/trajectory.rs
+++ src/trajectory.rs
@@ -1,3 +1,21 @@
+//!
+//! A trajectory is a circular buffer containing vectors. It can be seen as the trajectory
+//! of a moving point, in this case, the vectors are the position vectors of the point.
+//!
+//! **Note :** Use the method `push` to add a point to a trajectory.
+//! ## Purpose
+//! This abstraction aims represent only the last points of a complete trajectory.
+//! It's main purpose is to allow drawing a trajectory consistently without allocating
+//! new space each time a point is added.
+//!
+//! ## Conventions
+//! * All the trajectories have the same fixed size
+//! * Vector space operations can be performed between trajectories
+//! * Translation can be performed with a vector using `+` and `-` operators
+//! * Scaling can be performed using `*` and `/` operators
+//! * When adding a point to a trajectory, the oldest point is erased
+//!
+
 use std::fmt;
 use std::fmt::Debug;
 use std::ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Sub, SubAssign};
@@ -6,30 +24,45 @@ use crate::prelude::{Initializer, Reset};
 use crate::vector::{Vector2, Vector3, Vector4};
 use crate::trajectory::consts::*;
 
+/// 2D trajectory
 pub type Trajectory2 = Trajectory<Vector2>;
+
+/// 3D trajectory
 pub type Trajectory3 = Trajectory<Vector3>;
+
+/// 4D trajectory
 pub type Trajectory4 = Trajectory<Vector4>;
 
+
+/// constant trajectories
 pub mod consts {
     use super::*;
     use crate::vector;
 
+
+    /// Size of all the trajectories
     pub const TRAJECTORY_SIZE: usize = 256;
 
+    /// 2D zero trajectory
     pub const ZEROS_2: Trajectory2 = Trajectory2 {
         positions: [vector::consts::ZEROS_2; TRAJECTORY_SIZE],
         index: 0,
     };
+
+    /// 3D zero trajectory
     pub const ZEROS_3: Trajectory3 = Trajectory3 {
         positions: [vector::consts::ZEROS_3; TRAJECTORY_SIZE],
         index: 0,
     };
+
+    /// 4D zero trajectory
     pub const ZEROS_4: Trajectory4 = Trajectory4 {
         positions: [vector::consts::ZEROS_4; TRAJECTORY_SIZE],
         index: 0,
     };
 }
 
+/// Generic structure and documentation for trajectories
 #[derive(Copy, Clone)]
 pub struct Trajectory<T> {
     positions: [T; TRAJECTORY_SIZE],
@@ -53,34 +86,42 @@ impl<T> From<[T; TRAJECTORY_SIZE]> for Trajectory<T> where
 
 impl<T> Trajectory<T> where
     T: Copy + Clone {
+
+    /// Construct a trajectory with given positions and index of the oldest position in the array
     #[inline]
     pub fn new(positions: [T; TRAJECTORY_SIZE], index: usize) -> Trajectory<T> {
         Trajectory { positions, index }
     }
 
+    /// Add a vector to the trajectory
     #[inline]
     pub fn push(&mut self, position: &T) {
         self.positions[self.index] = *position;
         self.index = self.index_offset(1);
     }
 
+    /// Get the positions vectors of the trajectory as array
     #[inline]
     pub fn positions(&self) -> &[T; TRAJECTORY_SIZE] { &self.positions }
 
+    /// Get the position vector at index. The 0 index is the oldest position vector
     #[inline]
     pub fn position(&self, i: usize) -> &T {
         &self.positions[self.index_offset(i)]
     }
 
+    /// Same as `position` but with a mutable reference
     #[inline]
     pub fn position_mut(&mut self, i: usize) -> &mut T {
         &mut self.positions[self.index_offset(i)]
     }
 
+    /// Get the lastly added position vector
     pub fn last(&self) -> &T {
         &self.positions[self.index_offset(TRAJECTORY_SIZE - 1)]
     }
 
+    /// Same as `last` but with a mutable reference
     pub fn last_mut(&mut self) -> &mut T {
         &mut self.positions[self.index_offset(TRAJECTORY_SIZE - 1)]
     }
